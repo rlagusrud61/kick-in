f673c2508d9590a1714c8fa6176f7eea8c6fc4e8
joepvorage <j.v.w.vorage@student.utwente.nl>
2020-06-10 19:00:17 +0200

    small check

--- kickIn_26/src/main/java/nl/utwente/di/team26/Product/dao/Events/EventsDao.java
+++ kickIn_26/src/main/java/nl/utwente/di/team26/Product/dao/Events/EventsDao.java
@@ -329,7 +329,6 @@ public class EventsDao {
      * @param stmt This parameter contains the SQL statement to be excuted.
      */
     protected int databaseUpdate(Connection conn, PreparedStatement stmt) throws SQLException {
-
         return stmt.executeUpdate();
     }
 
