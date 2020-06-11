0d5074d730bae14e0260e3b4b764fb5c85b79a18
puruvaish24 <puruvaish24@outlook.com>
2020-06-11 12:54:38 +0200

    Map creation update.

--- kickIn_26/src/main/java/nl/utwente/di/team26/Product/resources/Maps/MapsResource.java
+++ kickIn_26/src/main/java/nl/utwente/di/team26/Product/resources/Maps/MapsResource.java
@@ -1,22 +1,13 @@
 package nl.utwente.di.team26.Product.resources.Maps;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import nl.utwente.di.team26.CONSTANTS;
 import nl.utwente.di.team26.Product.dao.Maps.MapsDao;
 import nl.utwente.di.team26.Product.model.Map.Map;
 import nl.utwente.di.team26.Security.Filters.Secured;
 import nl.utwente.di.team26.Security.User.Roles;
-import nl.utwente.di.team26.Security.User.User;
-import nl.utwente.di.team26.Security.User.UserDao;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import java.security.Key;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -24,10 +15,9 @@ import java.util.List;
 @Path("/maps")
 public class MapsResource {
 
-    MapsDao mapsDao = new MapsDao();
 
-    @Context
-    HttpHeaders httpHeaders;
+
+    MapsDao mapsDao = new MapsDao();
 
     @GET
     @Secured(Roles.VISITOR)
@@ -46,28 +36,9 @@ public class MapsResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
     public String addNewMap(Map mapToAdd) {
-
-        String jwtToken = httpHeaders.getCookies().get(CONSTANTS.COOKIENAME).getValue();
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-        Key signingKey = new SecretKeySpec(CONSTANTS.SECRET.getBytes(), signatureAlgorithm.getJcaName());
-
-        int userId = Integer.parseInt(
-                Jwts.parserBuilder()
-                        .setSigningKey(signingKey)
-                        .build()
-                        .parseClaimsJws(jwtToken)
-                        .getBody()
-                        .getSubject());
-
-
         try (Connection conn = CONSTANTS.getConnection()) {
-            User user = (new UserDao()).getObject(conn, userId);
-
-            mapToAdd.setCreatedBy(user.getEmail());
-            mapToAdd.setLastEditedBy(user.getEmail());
-
             return String.valueOf(mapsDao.create(conn, mapToAdd));
-        } catch (SQLException | nl.utwente.di.team26.Exceptions.NotFoundException throwables) {
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
             return CONSTANTS.FAILURE + ": " + throwables.getMessage();
         }
