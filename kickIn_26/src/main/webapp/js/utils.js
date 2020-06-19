d06fcc870d6d2aa8338086422fec01bae0c8c74c
Priya Naguine <p.v.naguine@student.utwente.nl>
2020-06-19 13:13:38 +0200

    documentation and changes done

--- kickIn_26/src/main/webapp/js/utils.js
+++ kickIn_26/src/main/webapp/js/utils.js
@@ -1,10 +1,11 @@
 /**
- *
  * @param {string} tableId - The ID of the table to be sorted.
  *
- * @summary This method
+ * @param {string} sortCriteria - The table header on which the table is to be sorted.
+ *
+ * @summary This method sorts the table in ascending order using the bubble sort algorithm.
  */
-function sortTableAscending(tableId) {
+function sortTableAscending(tableId, sortCriteria) {
     let table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById(tableId);
     switching = true;
@@ -21,8 +22,13 @@ function sortTableAscending(tableId) {
             shouldSwitch = false;
             /* Get the two elements you want to compare,
             one from current row and one from the next: */
-            x = rows[i].getElementsByTagName("TD")[0];
-            y = rows[i + 1].getElementsByTagName("TD")[0];
+            if (sortCriteria === "name" || sortCriteria === "event"){
+                x = rows[i].getElementsByTagName("TD")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[0];
+            } else if (sortCriteria === "date"){
+                x = rows[i].getElementsByTagName("TD")[1];
+                y = rows[i + 1].getElementsByTagName("TD")[1];
+            }
             // Check if the two rows should switch place:
             if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
                 // If so, mark as a switch and break the loop:
@@ -39,9 +45,16 @@ function sortTableAscending(tableId) {
     }
 }
 
-function sortTableDescending(tableId) {
+/**
+ * @param {string} tableId - The ID of the table to be sorted.
+ *
+ * @param {string} sortCriteria - Indication on the table column on which the table is to be sorted.
+ *
+ * @summary This method sorts the table in descending order using the bubble sort algorithm.
+ */
+function sortTableDescending(tableId, sortCriteria) {
     let table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("eventtable");
+    table = document.getElementById(tableId);
     switching = true;
     /* Make a loop that will continue until
     no switching has been done: */
@@ -56,8 +69,13 @@ function sortTableDescending(tableId) {
             shouldSwitch = false;
             /* Get the two elements you want to compare,
             one from current row and one from the next: */
-            x = rows[i].getElementsByTagName("TD")[0];
-            y = rows[i + 1].getElementsByTagName("TD")[0];
+            if (sortCriteria === "name" || sortCriteria === "event"){
+                x = rows[i].getElementsByTagName("TD")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[0];
+            } else if (sortCriteria === "date"){
+                x = rows[i].getElementsByTagName("TD")[1];
+                y = rows[i + 1].getElementsByTagName("TD")[1];
+            }
             // Check if the two rows should switch place:
             if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
                 // If so, mark as a switch and break the loop:
@@ -74,6 +92,12 @@ function sortTableDescending(tableId) {
     }
 }
 
+/**
+ * @summary This method is used to log out.
+ *
+ * @description A DELETE request is sent to the RESTful service provider with the given URL. The method then redirects
+ * the user to the 'login.html' page if the request was successful and takes the cookie data away from the user.
+ */
 function logout() {
     let xhr = new XMLHttpRequest();
     xhr.open('DELETE', "http://localhost:8080/kickInTeam26/rest/authentication", true);
@@ -87,6 +111,18 @@ function logout() {
     xhr.send();
 }
 
+/**
+ *
+ * @param {string} id - The ID of the element that takes input from the user.
+ *
+ * @summary This method is used to prevent Cross-Site Scripting (XSS).
+ *
+ * @description This method takes the value from the input element using its ID and checks whether the phrases 'onload',
+ * 'onerror', '<script>' and 'alert' are found. If they are found, the input element is emptied and an empty string is
+ * returned. If not, the value found is returned.
+ *
+ * @returns {string} - If the phrases are found, an empty string is returned. If not, the value found is returned.
+ */
 function XSSInputSanitation(id) {
     let element = document.getElementById(id).value;
     if (element.indexOf("onload") !== -1 || element.indexOf("<script>") !== -1 ||
@@ -98,10 +134,24 @@ function XSSInputSanitation(id) {
     }
 }
 
+/**
+ * @summary This method is used to go back to the previously accessed page within the website.
+ */
 function goBack() {
     window.history.back();
 }
 
+/**
+ * @param {string} tableID - the ID of the table on which the search on the first column is to be done.
+ *
+ * @param {string} searchBoxID - the ID of the search input box which has the search criteria.
+ *
+ * @summary This method is used to search through the table for the required information.
+ *
+ * @description This method first sanitises the value in the search box to prevent cross site scripting. If the value is
+ * emptied, the whole table is shown. If not, the required data is shown on the table if found, and an empty table is
+ * shown if the required data was not found.
+ */
 function searchTables(tableID, searchBoxID) {
     // Declare variables
     let searchValue, filter, table, tr, td, i, txtValue;
