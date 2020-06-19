575079ce479b8f5d3c25d65cb8698b2ff2fb1b9c
Priya Naguine <p.v.naguine@student.utwente.nl>
2020-06-19 11:53:43 +0200

    JavaScript conventions and other changes

--- kickIn_26/src/main/webapp/js/newMap.js
+++ kickIn_26/src/main/webapp/js/newMap.js
@@ -1,27 +1,16 @@
-function XSSInputSanitation(id) {
-    let element = document.getElementById(id).value;
-    if (element.indexOf("onload") !== -1 || element.indexOf("<script>") !== -1 ||
-        element.indexOf("onerror") !== -1 || element.indexOf("alert") !== -1) {
-        console.log("done")
-        document.getElementById(id).value = "";
-    }
-}
-
 function addNewMap() {
-    let mapName = document.getElementById("mapName").value;
-    let description = document.getElementById("description").value;
-
-    let createMap = new XMLHttpRequest();
-    let mapId;
-    let eventId = window.location.search.split("=")[1];
-    let mapJSON = {
+    let mapName, description, mapId, eventId, mapJSON, eventMapJSON;
+    mapName = document.getElementById("mapName").value;
+    description = document.getElementById("description").value;
+    eventId = window.location.search.split("=")[1];
+    mapJSON = {
     		"name" : mapName,
     		"description" : description
     }
     console.log(mapJSON)
     addMap(mapJSON, function() {
     	mapId = parseInt(this.responseText);
-    	let eventMapJSON = {
+    	eventMapJSON = {
     			"eventId": eventId,
     			"mapId": mapId
     	}
@@ -29,8 +18,4 @@ function addNewMap() {
     		window.location.href = "http://localhost:8080/kickInTeam26/event.html?id=" + eventId;
     	})
     })
-}
-
-function goBack() {
-    window.history.back();
 }
