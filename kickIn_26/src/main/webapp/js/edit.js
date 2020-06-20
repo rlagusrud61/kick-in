9e47891db04f3f7fcac966365b55fc530c659218
Priya Naguine <p.v.naguine@student.utwente.nl>
2020-06-20 15:55:16 +0200

    documentation added

--- kickIn_26/src/main/webapp/js/edit.js
+++ kickIn_26/src/main/webapp/js/edit.js
@@ -1,9 +1,17 @@
+/**
+ * @summary This method is used to update the information on a specific event.
+ *
+ * @description The values of the description, name, location and date of the event are retrieved from the web page
+ * and the ID of the event to be updated is retrieved from the URL. This information is then used to create a JSON
+ * object which is taken as a parameter to the function 'updateEvent' so that the information on the event can be
+ * updated in the database. Once the information is updated, the user is redirected to the page of the specific event.
+ */
 function updateEventData() {
 	let eventDescription, eventId, eventName, eventLoc, eventLocation, eventDate, eventJSON
-    eventDescription = document.getElementById("eventdescription").value;
+    eventDescription = document.getElementById("eventDescription").value;
     eventId = window.location.search.split("=")[1];
-    eventName = document.getElementById("eventname").value;
-    eventLoc = document.getElementById("eventlocation");
+    eventName = document.getElementById("eventName").value;
+    eventLoc = document.getElementById("eventLocation");
     eventLocation = eventLoc.options[eventLoc.selectedIndex].value
     eventDate = document.getElementById("eventDate").value;
     eventJSON = {
--- kickIn_26/src/main/webapp/js/login.js
+++ kickIn_26/src/main/webapp/js/login.js
@@ -1,8 +1,22 @@
+/**
+ * @summary This function is used to log into the website.
+ *
+ * @description First, the email address and password of the user are retrieved from the web page. A JSON object is
+ * then created from the data that was retrieved. A POST request is sent to the RESTful service provider with the given
+ * URL, where the content of the body is the JSON object that was created using the email address and the password.
+ * The method then redirects the user to the 'list.html' page if the authentication was a success.
+ */
 function login() {
 
-    let emailAddress, password, xhr;
+    let emailAddress, password, xhr, credentialsJSON;
+
     emailAddress = document.getElementById("inputEmail").value;
     password = document.getElementById("inputPassword").value;
+
+    credentialsJSON = {
+        "email": emailAddress,
+        "password": password
+    };
 
     console.log(emailAddress);
     console.log(password);
@@ -23,13 +37,20 @@ function login() {
 
     xhr.open('POST', "http://localhost:8080/kickInTeam26/rest/authentication", true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send('{"email": "' + emailAddress + '", "password": "' + password + '"}');
+    xhr.send(JSON.stringify(credentialsJSON));
 }
 
+/**
+ * @summary This method allows the user to login by pressing the enter key when both the email address and password
+ * have been entered.
+ *
+ * @desccription This method first checks the password input field for any signs of an XSS attack. After,
+ */
 function checkKeyPress() {
-    let password;
+    let password, emailAddress;
     password = XSSInputSanitation('inputPassword');
-	if (event.keyCode === 13 && password !== "") {
+    emailAddress = document.getElementById('inputEmail').value;
+	if (event.keyCode === 13 && password !== "" && emailAddress !== "") {
 		login();
 	}
 }
