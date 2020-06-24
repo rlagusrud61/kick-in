2d412e54c29289025298502274d97e1395ee1c28
puruvaish24 <puruvaish24@outlook.com>
2020-06-24 15:05:50 +0200

    UPDATE: mapView page done

--- kickIn_26/src/main/webapp/js/mapView.js
+++ kickIn_26/src/main/webapp/js/mapView.js
@@ -1,5 +1,5 @@
-let mapId, modal, trash, closeDelete, closeDelete2, map, tiles, printer, editToggle, imgGroup, inFreeHand, materialsList,
-    modal2, share, closeExport, closeExport2;
+let mapId, modal, trash, closeDelete, closeDelete2, map, printer, imgGroup,
+    modal2;
 
 mapId = window.location.search.split("=")[1];
 // Get the modal
@@ -25,35 +25,41 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
-//Modal for exporting the map
-
-//Get modal
-// modal2 = document.getElementById("modalExport");
-//Button for exporting
-// share = document.getElementById("share");
-//Closing popup
-// closeExport = document.getElementsByClassName("close")[1];
-//Closing by pressing "No"
-// closeExport2 = document.getElementById("no1");
-// share.onclick = function () {
-//     modal2.style.display = "block";
-// }
-// closeExport.onclick = function () {
-//     modal2.style.display = "none";
-// }
-// closeExport2.onclick = function () {
-//     modal2.style.display = "none";
-// }
 window.onclick = function (event) {
     if (event.target === modal2) {
         modal2.style.display = "none";
     }
 }
 
+function initMap() {
+    let newMap = L.map('mapid', {
+        center: [52.2413, -353.1531],
+        zoom: 16,
+        keyboard: false,
+        fullscreenControl: true,
+        fullscreenControlOptions: {
+            position: 'topleft'
+        }
+    });
+    let tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    tiles.addTo(newMap);
+    return newMap;
+}
+function parseLatLangs(stringyLatLangs) {
+    let latLangArray = JSON.parse(stringyLatLangs);
+    let coords = [];
+    latLangArray.forEach(function (point) {
+        coords.push(L.latLng(point.lat, point.lng))
+    })
+    return coords;
+}
+
 //inits the map and properties.
 map = initMap();
 printer = L.easyPrint({
-    tileLayer: tiles,
     sizeModes: ['Current'],
     filename: 'myMap',
     exportOnly: true,
@@ -81,6 +87,7 @@ function insertObjectsToMap() {
 function insertObjectIntoMap(object) {
     let corners = parseLatLangs(object.latLangs);
     let newImage = L.distortableImageOverlay(images.get(object.resourceId), {
+        editable: false,
         actions: [],
         corners: corners
     })
@@ -105,12 +112,13 @@ function bringAllResources() {
  */
 function initPage() {
     getMap(mapId, function () {
+        bringAllResources();
         let mapEditBtn = document.getElementById("mapEditBtn");
         mapEditBtn.href = "mapEdit.html?mapId=" + mapId;
         let mapData = JSON.parse(this.responseText);
         document.getElementById("mapName").innerHTML = mapData.name;
         document.getElementById("description").innerHTML = mapData.description;
-
+        listItems(mapData.report);
     })
 }
 
@@ -125,67 +133,29 @@ window.onload = initPage;
  * the information received. This table has two columns 'Name' and 'Quantity', where the column 'Name' gives the name of
  * the item that was placed on the map and the column 'Quantity' gives the count of each item that was placed on the map.
  */
-function listItems() {
+function listItems(report) {
+    let returnedItems, col, key, table, th, tr, i, j, tableCell;
+    returnedItems = report;
+    col = ["Name", "Count"];
+    table = document.createElement("table"); // creates the table
+    table.setAttribute("id", "resources")
+    table.setAttribute("class", "table table-hover")
+    tr = table.insertRow(-1); // add a row to the table
 
-    let mapId, returnedItems, col, key, table, th, tr, i, j, tableCell;
-    mapId = window.location.search.split("=")[1];
-    generateReportForMap(mapId, function () {
+    for (i = 0; i < col.length; i++) {
+        th = document.createElement("th"); // add a header to the table
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
 
-        returnedItems = JSON.parse(this.responseText);
-
-        col = [];
-        for (i = 0; i < returnedItems.length; i++) {
-            for (key in returnedItems[i]) {
-                if (col.indexOf(key) === -1 && (key === 'name' || key === 'count')) {
-                    if (key === 'name'){
-                        col.push('Name');
-                    } else if (key === 'count'){
-                        col.push('Count');
-                    }
-                }
-            }
+    for (i = 0; i < returnedItems.length; i++) {
+        tr = table.insertRow(-1); // adds a new row
+        for (j = 0; j < col.length; j++) {
+            tableCell = tr.insertCell(-1);
+            tableCell.innerHTML = returnedItems[i][col[j].toLowerCase()]; // adds the required data to the table
         }
-
-        table = document.createElement("table"); // creates the table
-        table.setAttribute("id", "resources")
-        table.setAttribute("class", "table table-hover")
-        tr = table.insertRow(-1); // add a row to the table
-
-        for (i = 0; i < col.length; i++) {
-            th = document.createElement("th"); // add a header to the table
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
-
-        for (i = 0; i < returnedItems.length; i++) {
-
-            tr = table.insertRow(-1); // adds a new row
-            for (j = 0; j < col.length; j++) {
-                tableCell = tr.insertCell(-1);
-                tableCell.innerHTML = returnedItems[i][col[j].toLowerCase()]; // adds the required data to the table
-            }
-        }
-        document.getElementById("listItems").appendChild(table);
-    });
-}
-
-/**
- * @summary This method is used to get the name and description of the map required.
- *
- * @description After the ID of the required map is retrieved from the URL, the 'getMap' function is called with the ID
- * as a parameter. If the map information is successfully retrieved from the back-end, the name and description of the
- * map are displayed on the page where required.
- */
-function getMapNameAndDescription() {
-    let mapId, jsonData, mapEditBtn;
-    mapId = window.location.search.split("=")[1];
-    mapEditBtn = document.getElementById("mapEditBtn");
-    mapEditBtn.href = "mapEdit.html?mapId=" + mapId;
-    getMap(mapId, function () {
-        jsonData = JSON.parse(this.responseText);
-        document.getElementById("mapName").innerHTML = jsonData.name;
-        document.getElementById("description").innerHTML = jsonData.description;
-    })
+    }
+    document.getElementById("listItems").appendChild(table);
 }
 
 /**
@@ -201,20 +171,3 @@ function removeMap() {
         window.location.href = "list.html";
     })
 }
-
-/**
- *
- *  This part of the code is used for taking screenshots of the map
- *
- *  Exporting in png format
- */
-document.getElementById("yes1").addEventListener('click', function()
-{
-    html2canvas(document.getElementsByClassName("specific")[0]).then(function (canvas) {
-
-            //document.body.appendChild(canvas);
-            modal2.style.display = "none";
-
-            return Canvas2Image.saveAsPNG(canvas);
-    });
-});
