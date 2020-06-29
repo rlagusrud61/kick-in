70e3064913a7134a8bb67be907ac5570541a2a3a
Priya Naguine <p.v.naguine@student.utwente.nl>
2020-06-29 18:40:20 +0200

    Update: TestEvent.java and AdminNavigationTest.java fixed.

--- kickIn_26/src/test/java/SeleniumTests/AdminNavigationTest.java
+++ kickIn_26/src/test/java/SeleniumTests/AdminNavigationTest.java
@@ -46,16 +46,19 @@ public class AdminNavigationTest {
   public void adminNavigation() throws InterruptedException {
     driver.get("http://localhost:8080/kickInTeam26/");
 
+    // Log in.
     driver.findElement(By.id("inputEmail")).sendKeys("admin@gmail.com");
     driver.findElement(By.id("inputPassword")).sendKeys("adminpass");
     driver.findElement(By.id("inputPassword")).sendKeys(Keys.ENTER);
     Thread.sleep(7000);
 
+    // Add a new event.
     driver.findElement(By.id("addEventBtn")).click();
     driver.findElement(By.id("eventName")).click();
     driver.findElement(By.id("eventName")).sendKeys("Adding Event");
+    driver.findElement(By.id("eventName")).sendKeys(Keys.TAB);
     driver.findElement(By.id("eventDescription")).sendKeys("As an Admin");
-    driver.findElement(By.id("eventDate")).click();
+    driver.findElement(By.id("eventDescription")).sendKeys(Keys.TAB);
     driver.findElement(By.id("eventDate")).clear();
     driver.findElement(By.id("eventDate")).sendKeys("2020-06-24");
     driver.findElement(By.id("eventLocation")).click();
@@ -66,19 +69,16 @@ public class AdminNavigationTest {
     driver.findElement(By.cssSelector(".bottomright > .align-self-end")).click();
     Thread.sleep(7000);
 
-    driver.findElement(By.id("deleteEvent")).click();
-    Thread.sleep(7000);
-
-    driver.findElement(By.id("home")).click();
-    Thread.sleep(7000);
-
+    // Delete an event.
     driver.findElement(By.cssSelector("tr:nth-child(5) .glyphicon-trash")).click();
     driver.findElement(By.id("yesDeleteButton")).click();
     Thread.sleep(7000);
 
+    // Go to the users page.
     driver.findElement(By.cssSelector(".glyphicon-user")).click();
     Thread.sleep(7000);
 
+    // Add a user.
     driver.findElement(By.id("myBtn")).click();
     driver.findElement(By.id("userName")).click();
     driver.findElement(By.id("userName")).sendKeys("NewUser");
@@ -93,9 +93,11 @@ public class AdminNavigationTest {
     driver.findElement(By.cssSelector(".bottomright > .align-self-end")).click();
     Thread.sleep(7000);
 
+    // Go to the maps page.
     driver.findElement(By.cssSelector(".glyphicon-globe")).click();
     Thread.sleep(7000);
 
+    // Add a map.
     driver.findElement(By.id("addMapBtn")).click();
     driver.findElement(By.id("mapName")).click();
     driver.findElement(By.id("mapName")).sendKeys("New Map");
@@ -103,16 +105,20 @@ public class AdminNavigationTest {
     driver.findElement(By.cssSelector(".bottomright > .align-self-end")).click();
     Thread.sleep(7000);
 
-    driver.findElement(By.cssSelector("tr:nth-child(24) .glyphicon-trash")).click();
+    // Delete a map.
+    driver.findElement(By.cssSelector("tr:nth-child(2) .glyphicon-trash")).click();
     driver.findElement(By.id("yesDeleteButton")).click();
     Thread.sleep(7000);
 
+    // Go to the resources page.
     driver.findElement(By.cssSelector(".glyphicon-cog")).click();
     Thread.sleep(7000);
 
+    // Go back to the list of events page.
     driver.findElement(By.cssSelector(".glyphicon-home")).click();
     Thread.sleep(7000);
 
+    // Log out.
     driver.findElement(By.id("logout")).click();
     Thread.sleep(7000);
   }
